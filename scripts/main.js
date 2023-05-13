86d784e0c02de7678138362457e71f1110c16217
hirwaNkevin <hirwankevin@gmail.com>
2023-05-13 20:49:21 +0200

    able to add statement using script

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,2 @@
-alert("hello!")
+document.getElementById("chat-messages").innerHTML='<p class="statement">Hi, I am Mike.</p>';
+document.getElementById("chat-messages").innerHTML='<p class="statement">I am supposed to be funny. waiting for you to laugh...</p>';

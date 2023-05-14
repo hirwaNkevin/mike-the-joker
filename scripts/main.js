768b05c2a006cd8790603367b6c0a943e66d69ce
hirwaNkevin <hirwankevin@gmail.com>
2023-05-14 19:58:03 +0200

    clean mainjs

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-// document.getElementById("chat-messages").innerHTML='<p class="statement">Hi, I am Mike.</p>';
-
 function respond(message){
     document.getElementById("chat-messages").innerHTML='<p class="statement">'+message+'</p>';
 }

83dd5ad042b1e7366c1f97328d3126146e1b5202
hirwaNkevin <hirwankevin@gmail.com>
2023-05-14 19:54:57 +0200

    make output methods/functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
-document.getElementById("chat-messages").innerHTML='<p class="statement">Hi, I am Mike.</p>';
-document.getElementById("chat-messages").innerHTML='<p class="statement">I am supposed to be funny. waiting for you to laugh...</p>';
+// document.getElementById("chat-messages").innerHTML='<p class="statement">Hi, I am Mike.</p>';
+
+function respond(message){
+    document.getElementById("chat-messages").innerHTML='<p class="statement">'+message+'</p>';
+}
+
+respond("Hello!")
+
+function ask(prompt){
+    document.getElementById("chat-messages").innerHTML='<p class="user-statement statement">'+prompt+'</p>';
+}
+
+ask("hi")

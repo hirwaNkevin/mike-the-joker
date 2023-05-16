10c861f272c2b92c1b62fd77be7139b466dc8c70
hirwaNkevin <hirwankevin@gmail.com>
2023-05-16 22:58:20 +0200

    make mike say more

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
+
+respond("Hi, I'm Mike!")
+respond("I am supposed to make you laugh in the seconds displayed.")
+respond("Are you ready?!")
+
 function respond(message){
     let statement = document.createElement("p");
     statement.className= "statement";
     statement.innerHTML = message;
     document.getElementById("chat-messages").appendChild(statement);
 }
-
-respond("Hello!")
 
 function ask(prompt){
     let statement = document.createElement("p");
